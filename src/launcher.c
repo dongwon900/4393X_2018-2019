f9372414b591d2ec8d3e35186259a0ee16446ae0
Jack Kang <dongwon900@gmail.com>
2018-11-04 14:46:40 -0500

    auto-cocking function added

--- src/launcher.c
+++ src/launcher.c
@@ -1,12 +1,31 @@
 #include "main.h"
 #include "launcher.h"
 
-void launcher(){
-  bool launch = joystickGetDigital(1, 6, JOY_DOWN);
-  if(launch){
+static bool isCocked = false; //MAKE THIS INITIALIZE TO THE STATE OF THE LIMIT SWITCH INSTEAD OF ALWAYS AT FALSE
+
+
+
+void launcher() {
+  bool launch = joystickGetDigital(1, 8, JOY_DOWN);
+  // if limit switch not pressed down, continue reloading
+  if (digitalRead(LIMIT_SWITCH) == HIGH) {
     motorSet(6, 127);
+    isCocked = false;
   }
-  else{
-    motorSet(6,0);
+  // else, give control back to user for shooting
+  else {
+    if (isCocked == false) {
+      //lcdSetText(uart1, 1, );
+      motorSet(6, 127);
+      delay(250);
+      isCocked = true;
+      motorSet(6, 0);
+    }
+
+    if (launch) {
+      motorSet(6, 127);
+    } else {
+      motorSet(6,0);
+    }
   }
 }
