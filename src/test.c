ef3d42db79bd8ed2ef4b14bcfa9378633e8bd96c
dongwon900 <dongwon900@gmail.com>
2018-11-09 10:18:02 -0500

    yote

--- src/test.c
+++ src/test.c
@@ -1,29 +1,28 @@
 #include <main.h>
 #include <test.h>
+#include <stdlib.h>
 
   int *ime_0;
   int *ime_1;
 
 void test(){
   // arm readings
-  print("\nArm: ");
-  print((const char*) analogRead(1));
+  printf("Arm: %d \n", analogRead(1));
   // claw readings
-  print("\nClaw: ");
-  print((const char*) analogRead(2));
+  printf("Claw: %d \n", analogRead(2));
 
-  // left line tracker readings
-  print("\nLeft_linetracker: ");
-  print((const char*) analogRead(3));
-  // right line tracker readings
-  print("\nRight_linetracker: ");
-  print((const char*) analogRead(4));
+  // left and right line tracker readings
+  printf("Left Line Tracker: %d \n", analogRead(3));
+  printf("Right Line Tracker: %d \n", analogRead(4));
+
 
   // left and right encoder readings
   imeGet(0,ime_0);
   imeGet(0,ime_1);
-  printf("left encoder count: %d, right encoder count: %d", *ime_0, *ime_1);
+  printf("left encoder count: %d, right encoder count: %d \n", *ime_0, *ime_1);
 
+  delay(750);
+  system("clear");
 };
 
 // int menu_state = 0;
